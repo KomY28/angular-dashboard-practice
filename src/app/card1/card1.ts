f34eb3f4a514035bf0dadf642ef2bcd9d7af998e
KomY <ko.bal1nt03@gmail.com>
2026-07-11 09:24:41 +0200

    feat: implement form submission and form reset logic using ViewChild

--- src/app/card1/card1.ts
+++ src/app/card1/card1.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { OkosMezo } from '../okos-mezo/okos-mezo';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-card1',
-  imports: [OkosMezo],
+  imports: [OkosMezo, FormsModule],
   templateUrl: './card1.html',
   styleUrl: './card1.css',
 })
-export class Card1 {}
+export class Card1 {
+  @ViewChild("form") form?: ElementRef<HTMLFormElement>;
+  onSubmit(usernameElement: string, descriptionElement: string)
+  {
+    console.log(usernameElement + " " + descriptionElement);
+    this.form?.nativeElement.reset()
+  }
+}
